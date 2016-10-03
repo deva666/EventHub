16f2c738465e118cd82c7d9bdde9d39ab296ab99
Marko Devcic <madevcic@gmail.com>
2016-10-03 21:33:30 +0200

    add token javadocs

--- app/src/main/java/com/markodevcic/eventhub/Token.java
+++ app/src/main/java/com/markodevcic/eventhub/Token.java
@@ -1,7 +1,17 @@
 package com.markodevcic.eventhub;
 
+/**
+ * Token returns from {@link EventHub#subscribeForToken(Class, OnEvent)} for releasing references and unsubscribing from events
+ */
 public interface Token {
+
+	/**
+	 * Release the reference and stop receiving event notifications
+	 */
     void unSubscribe();
 
+	/**
+	 * Is this token still subscribed
+	 */
     boolean isSubscribed();
 }
